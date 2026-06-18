5aa0cb01d2d36a67c2a57ac8efe20d7189dab1d9
juursn <anajuliaramos343@gmail.com>
2026-06-18 01:10:24 -0300

    Corrigindo deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import "./App.css";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./App.css"
+import { Route, BrowserRouter, Routes } from "react-router-dom"
 
-import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
-import { Home } from "./pages/Home/Home";
+import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer"
+import { Home } from "./pages/Home/Home"
 
-export const App = () =>{
-  return(
-    <> 
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/HORTA-ESCOLAR" element={<Home/>}/>
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-    </>
-  )
+export const App = () => {
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/HORTA-ESCOLAR" element={<Home />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</>
+	)
 }
