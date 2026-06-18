b3246b0d32202d79572a80dacd9fc374a6c543e5
juursn <anajuliaramos343@gmail.com>
2026-06-17 23:43:46 -0300

    adiction[Home.tsx,div]: componente catálogo de plantas estilizado e com imagens (sem rotas temporariamente). Edições de estilo na page Home.tsx. Regras para h1,h2 adicionadas css geral.

--- src/components/divCatalogo.tsx
+++ src/components/divCatalogo.tsx
@@ -1,11 +1,26 @@
 import { NavLink } from "react-router-dom"
 
-export const DivCatalogo = ({txt, caminho}: {txt: string; caminho: string}) => {
-    return(
-        <>
-            <NavLink to={caminho}>
-                <p>{txt}</p>
-            </NavLink>
-        </>
-    )
+export const DivCatalogo = ({
+	txt,
+	caminho,
+	imagem,
+}: {
+	txt: string
+	caminho: string
+	imagem: string
+}) => {
+	return (
+		<>
+			<NavLink to={caminho}>
+				<div
+					className="bg-cover bg-center h-40 w-40 rounded-md flex justify-center items-end py-2"
+					style={{
+						backgroundImage: `url(${imagem})`,
+					}}
+				>
+					<p className="bg-white w-9/12 rounded-2xl h-fit py-0.5">{txt}</p>
+				</div>
+			</NavLink>
+		</>
+	)
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,43 @@
-import { dianaPNG } from "../../assets/index";
-import { DivCatalogo } from "../../components/divCatalogo";
+import {
+	alimenticia,
+	carnivora,
+	dianaPNG,
+	medicinal,
+	ornamental,
+} from "../../assets/index"
+import { DivCatalogo } from "../../components/divCatalogo"
 
 export const Home = () => {
-    return (
-        <>
-            <main>
-                {/* == BANNER PRINCIPAL == */}
-                <h1>Horta Escolar Digital</h1>
-                <section>
-                    <img src={dianaPNG} alt="Diana" />
-                    <div>
-                        <p>
-                            A Horta Escolar Digital é uma plataforma interativa criada para
-                            aproximar tecnologia, educação e sustentabilidade.
-                        </p>
-                    </div>
-                </section>
+	return (
+		<>
+			<main>
+				{/* == BANNER PRINCIPAL == */}
+				<h1 className="bg-linear-to-r from-green-400 to-green-700 bg-clip-text text-transparent text-4xl">
+					Horta Escolar Digital
+				</h1>
+				<section className="flex items-center px-3 bg-[#F5F5F773]">
+					<img src={dianaPNG} alt="Diana" />
+					<div className="bg-green-400 rounded-md h-fit py-4 px-2">
+						<p>
+							A Horta Escolar Digital é uma plataforma interativa criada para
+							aproximar tecnologia, educação e sustentabilidade.
+						</p>
+					</div>
+				</section>
 
-                {/* == CATÁLOGO DE PLANTAS == */}
-                <section>
-                    <h2>Catálogo de plantas</h2>
-                    <div>
-                        <DivCatalogo txt="Alimentícia" caminho="" />
-                        <DivCatalogo txt="Medicinal" caminho="" />
-                        <DivCatalogo txt="Ornamentar" caminho="" />
-                        <DivCatalogo txt="Carnívora" caminho="" />
-                    </div>
-                </section>
-            </main>
-        </>
-    );
+				{/* == CATÁLOGO DE PLANTAS == */}
+				<section className="bg[#F5F5F773]">
+					<h2 className="bg-linear-to-r from-purple-400 to-violet-700 bg-clip-text text-transparent text-3xl">
+						Catálogo de plantas
+					</h2>
+					<div className="text-center flex flex-wrap gap-3 items-center justify-center">
+						<DivCatalogo txt="Alimentícia" caminho="" imagem={alimenticia} />
+						<DivCatalogo txt="Medicinal" caminho="" imagem={medicinal} />
+						<DivCatalogo txt="Ornamentar" caminho="" imagem={ornamental} />
+						<DivCatalogo txt="Carnívora" caminho="" imagem={carnivora} />
+					</div>
+				</section>
+			</main>
+		</>
+	)
 }
